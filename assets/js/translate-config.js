19a41c7e228e63cc223911f4deb5febb8fe52aa3
iJimday <ijimday@gmail.com>
2026-05-17 18:51:22 +0800

    update051712
    不折腾了，就这样吧

--- assets/js/translate-config.js
+++ assets/js/translate-config.js
@@ -11,7 +11,7 @@ translate.nomenclature.append('chinese_simplified', 'english', `
 	小戴米铺=Jimday's Domains
 	欲买桂花同载酒，终不似，少年游。=I want to buy osmanthus to go drinking,but after all, I don’t have the traveling spirit at youth.
 	我的站点=My Websites
-	6个=6 Sites
+	6个 =6 Sites
 	我 =Me
 	菜 =Cai
 	小趴菜=Little Pacai
@@ -19,6 +19,7 @@ translate.nomenclature.append('chinese_simplified', 'english', `
 	米市=Domains Shop
 	小戴米市=Jimday's Domains Shop
 	铺 =Mipu
+	店 =Shop
 	测 =IP
 	链 =URL
 	短链接=Link Shortener
@@ -27,6 +28,8 @@ translate.nomenclature.append('chinese_simplified', 'english', `
 	本站模板源自 caonb.cn 原版魔改优化，由豆包负责疯狂的微微微微微微微微微微微微微微微调，Jimday 匠心打磨设计，仅限个人玩耍使用，未经允许严禁转载、拿去商用哦🈲！=This template is modified and optimized based on the original version from caonb.cn. It has been endlessly tweaked and finely adjusted by Doubao. Exquisitely designed and polished by Jimday. For personal use only. Unauthorized reproduction and commercial use are strictly prohibited 🚫!
 	明白明白=Got it, got it.
 	网站地图=Sitemap
+	试 =Test
+	4个 =4 Tools
 `); //自定义翻译术语
 translate.ignore.text.push('中文'); //翻译时忽略指定的文字不翻译
 translate.ignore.text.push('English'); //翻译时忽略指定的文字不翻译
