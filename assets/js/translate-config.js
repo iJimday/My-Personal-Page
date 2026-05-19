8aa1d2e983869a156e38ee4726eaa6ce2525e484
iJimday <ijimday@gmail.com>
2026-05-19 20:38:23 +0800

    update0519
    更新main.js、sitemap、index、tools
    translate.js-config添加诗词翻译

--- assets/js/translate-config.js
+++ assets/js/translate-config.js
@@ -7,8 +7,9 @@ translate.language.translateLanguagesRange = ['chinese_simplified']; //只翻译
 translate.nomenclature.append('chinese_simplified', 'english', `
 	戴=Dai
 	阿戴=Mr.Dai
-	小戴=Jimday
-	小戴米铺=Jimday's Domains
+	阿戴先生=Mr.Dai
+	小戴=JimDay
+	小戴米铺=JimDay's Domains
 	欲买桂花同载酒，终不似，少年游。=I want to buy osmanthus to go drinking,but after all, I don’t have the traveling spirit at youth.
 	我的站点=My Websites
 	6个 =6 Sites
@@ -18,7 +19,7 @@ translate.nomenclature.append('chinese_simplified', 'english', `
 	小趴菜=Little Pacai
 	米 =Mish
 	米市=Domains Shop
-	小戴米市=Jimday's Domains Shop
+	小戴米市=JimDay's Domains Shop
 	铺 =Mipu
 	店 =Shop
 	链 =URL
@@ -27,7 +28,7 @@ translate.nomenclature.append('chinese_simplified', 'english', `
 	短链接=Link Shortener
 	豆包=Doubao
 	微微微微微微微微微微微微微微微调=Fine-tuning code
-	本站模板源自 caonb.cn 原版魔改优化，由豆包负责疯狂的微微微微微微微微微微微微微微微调，Jimday 匠心打磨设计，仅限个人玩耍使用，未经允许严禁转载、拿去商用哦🈲！=This template is modified and optimized based on the original version from caonb.cn. It has been endlessly tweaked and finely adjusted by Doubao. Exquisitely designed and polished by Jimday. For personal use only. Unauthorized reproduction and commercial use are strictly prohibited 🚫!
+	本站模板与界面源自 大佬论坛逆袭 原版魔改优化，由豆包负责疯狂的微微微微微微微微微微微微微微微调，阿戴先生 匠心打磨设计，仅限个人玩耍使用，未经允许严禁转载、拿去商用哦🈲！=This template and UI of my website is modified and optimized based on the original version from dalao.net:NiXi. It has been endlessly tweaked and finely adjusted by Doubao. Exquisitely designed and polished by JimDay(Mr.Dai). For personal use only. Unauthorized reproduction and commercial use are strictly prohibited 🚫!
 	明白明白=Got it, got it.
 	网站地图=Sitemap
 	我的工具=My Tools
@@ -35,6 +36,8 @@ translate.nomenclature.append('chinese_simplified', 'english', `
 	试 =Test
 	测试=test
 	4个 =4 Tools
+	众里寻他千百度。蓦然回首，那人却在，灯火阑珊处。=I look repeatedly for her in vain in the crowd, Suddenly looking back, I find her,In the dim lantern light.
+	天生我材必有用，千金散尽还复来。=Heaven has made us talents, we're not made in vain. A thousand gold coins spent, more will turn up again.
 `); //自定义翻译术语
 translate.ignore.text.push('中文'); //翻译时忽略指定的文字不翻译
 translate.ignore.text.push('English'); //翻译时忽略指定的文字不翻译
